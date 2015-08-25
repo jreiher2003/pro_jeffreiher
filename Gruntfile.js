8bdcb0b6ffda11d0dd3db3ec69ec9def7eafd739
Jeff Reiher <jeffreiher@gmail.com>
2015-08-25 16:40:04 -0400

    add npm packages and configured Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+/*
+ After you have changed the settings under responsive_images
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            /* Change these */
+            width: 800,
+            suffix: '_large',
+          },{
+            width: 400,
+            suffix: '_medium',
+          },{
+            width: 200,
+            suffix: '_small',
+            quality: 30
+          }]
+        },
+
+        /*
+        You don't need to change this part if you don't change
+        the directory structure.
+        */
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png,svg}'],
+          cwd: 'jreiher/static/images/images_src/',
+          dest: 'jreiher/static/images/images_build/'
+        }]
+      }
+    },
+
+    /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['jreiher/static/images/images_build'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['jreiher/static/images/images_build']
+        },
+      },
+    },
+    imagemin: {
+      dev: {
+        options: {
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          src: ['*.{jpg,webp}'],
+          cwd: 'jreiher/static/images/images_build',
+          dest: 'jreiher/static/images/images_dest'
+        }]
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-contrib-imagemin')
+  grunt.registerTask('img', ['clean', 'mkdir','responsive_images']);
+  grunt.registerTask('min', ['imagemin'])
+
+};
