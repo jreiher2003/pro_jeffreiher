b051c6d2153b5f343746faa95a3e61a637dfb056
Jeff Reiher <jeffreiher@gmail.com>
2015-10-07 16:25:10 -0400

    add /blog and /project to site

--- jreiher/views.py
+++ jreiher/views.py
@@ -2,6 +2,15 @@ from flask import render_template
 from jreiher import app
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html')
     # return 'Hello Jeffrey'
+
+@app.route('/blog')
+def blog():
+	return "Hello blog"
+
+@app.route('/projects')
+def project():
+	return "Hello projects"
